e70ecc8a9b647b488bb9639fd448c750edad9eae
yedu.madhumanchi <yedu.madhumanchi@yondrgroup.com>
2021-12-15 13:26:37 +0530

    Add project files.

---
+++ GenericAPI/TaskCreateModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericAPI
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string CreatedOn { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+    public class CreateTaskModel
+    {
+        public string CreatedOn { get; set; }
+        public string Description { get; set; }
+    }
+    public class UpdateTaskModel
+    {
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
